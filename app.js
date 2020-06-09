2b4448447e03ca2fe1a42d7a77282da47e0ea850
Васильев Михаил <madgnu@madg.nu>
2020-06-09 09:16:18 +0300

    Add server code
    * Add server code using express.js framework
    * Add new routes /cards and /users
    * Add test, run and dev npm commands

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cardsRouter = require('./routes/cards');
+const usersRouter = require('./routes/users');
+
+const { HTTP_PORT = 3000, NODE_ENV = 'prod' } = process.env;
+const app = express();
+
+const logger = (req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.log(new Date(), req.ip, req.method, req.url, res.statusCode);
+  next();
+};
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/cards', cardsRouter);
+app.use('/users', usersRouter);
+if (NODE_ENV) app.use(logger);
+
+
+app.listen(HTTP_PORT);
